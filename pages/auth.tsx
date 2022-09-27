803818ac72d94662bb439278a45726ceb437d0c1
DevAndr <pitbullstar96@yandex.ru>
2022-09-28 01:55:04 +0300

    валидация формы auth

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -5,27 +5,35 @@ import React, {useEffect, useState} from 'react';
 import {useFormik} from 'formik';
 import {InputText} from 'primereact/inputtext';
 import {Button} from 'primereact/button';
-import {Dropdown} from 'primereact/dropdown';
 import {Calendar} from 'primereact/calendar';
 import {Password} from 'primereact/password';
 import {Checkbox} from 'primereact/checkbox';
 import {Dialog} from 'primereact/dialog';
 import {Divider} from 'primereact/divider';
 import {classNames} from 'primereact/utils';
+import { useRouter } from "next/router";
+import {useMutation, useQuery } from "@apollo/client";
+import {GET_USERS, LOG_IN, SIGN_UP} from "../services/graphql";
+import {ParamsAuth, ParamsSignUpAuth, RsponseAuth} from "../services/graphql/types";
+import {signIn, useSession} from "next-auth/react";
+import * as Yup from 'yup';
+import YupPassword from 'yup-password'
+YupPassword(Yup)
 
-export interface IErrorForm {
+interface IBaseDataForm {
     name?: string
     email?: string
     password?: string
-    accept?: string
 }
 
-export interface IDataForm {
-    name?: string
-    email?: string
-    date?: string | null
-    password?: string
+export interface IDataForm extends IBaseDataForm{
     accept?: boolean
+    dateOfBirth?: number
+}
+
+export interface IErrorForm extends IBaseDataForm {
+    accept?: string
+    dateOfBirth?: string
 }
 
 export enum TypeAuthView {
@@ -33,49 +41,54 @@ export enum TypeAuthView {
     SIGN_UP
 }
 
+const SignUpSchema = Yup.object().shape({
+    firstName: Yup.string()
+        .min(2, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required('Required'),
+    password: Yup.string().password()
+        .minLowercase(3, 'Пароль ложен содержать более 2 строчных символов')
+        .min(8, 'Минимальная длинна пароля 8')
+        .minUppercase(3, 'Пароль ложен содержать более 2 заглавных символов')
+        .minRepeating(2, 'Символ повторяется более 2 раз').required('Required'),
+    email: Yup.string().email('Invalid email').required('Required'),
+    dateOfBirth: Yup.date().required('Required'),
+    accept: Yup.boolean().isTrue().required('Required')
+});
+
+const LogInSchema = Yup.object().shape({
+    password: Yup.string().required('Введите пароль'),
+    email: Yup.string().email('Проверте email').required('Введите email'),
+});
+
 const AuthPage: NextPage = ({}) => {
+    const router = useRouter()
     const [showMessage, setShowMessage] = useState(false);
     const [formData, setFormData] = useState<IDataForm>({});
     const [typeView, setTypeView] = useState<TypeAuthView>(TypeAuthView.SIGN_IN)
     const formik = useFormik<IDataForm>({
-        initialValues: {
+        initialValues: typeView === TypeAuthView.SIGN_UP ? {
             name: '',
-            email: '',
-            password: '',
-            date: null,
+            email: 'biba@gmail.com',
+            password: 'qwerty',
+            dateOfBirth: undefined,
             accept: false
+        } : {
+            email: 'biba@gmail.com',
+            password: 'qwerty',
         },
-        validate: (data) => {
-            let errors: IErrorForm = {}
-
-            if (!data.name) {
-                errors.name = 'Введите имя';
-            }
-
-            if (!data.email) {
-                errors.email = 'Введите email';
-            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(data.email)) {
-                errors.email = 'Не верный форамат email. Пример example@email.com';
-            }
-
-            if (!data.password) {
-                errors.password = 'Введите пароль';
-            }
-
-            if (!data.accept) {
-                errors.accept = 'Я согласен с условиями и положениями';
-            }
-
-            return errors;
-        },
+        validationSchema: typeView === TypeAuthView.SIGN_UP ? SignUpSchema : LogInSchema,
         onSubmit: (data) => {
             console.log(data)
             setFormData(data);
             setShowMessage(true);
-
             formik.resetForm();
         }
     });
+    // const { loading, error, data } = useQuery(GET_USERS);
+
+    const [logInAuth] = useMutation<RsponseAuth, ParamsAuth>(LOG_IN);
+    const [signUpAuth] = useMutation<RsponseAuth, ParamsSignUpAuth>(SIGN_UP);
 
     // @ts-ignore
     const isFormFieldValid = (name: string) => !!(formik.touched[name] && formik.errors[name])
@@ -86,13 +99,39 @@ const AuthPage: NextPage = ({}) => {
     }
 
     const handleClosePage = () => {
-
+        setShowMessage(false)
+        router.push('/')
     }
 
-    const handleToggleView = () => {
+    const handleToggleView = async () => {
         setTypeView(prevState => {
             return prevState === TypeAuthView.SIGN_IN ? TypeAuthView.SIGN_UP : TypeAuthView.SIGN_IN
         })
+    }
+
+    const handleLogIn = async () => {
+        console.log(formData, formik)
+        const {data} = await logInAuth({
+            variables: {
+                data: {
+                    email: "biba@gmail.com",
+                    password: "qwerty"
+                }
+            }
+        })
+    }
+
+    const handleSignUp = async () => {
+        console.log(formData, formik)
+        // const {data} = await signUpAuth({
+        //     variables: {
+        //         data: {
+        //             name: "Kaka",
+        //             email: "biba@gmail.com",
+        //             password: "qwerty"
+        //         }
+        //     }
+        // })
     }
 
     const dialogFooter = <div className="flex justify-content-center"><Button label="OK" className="p-button-text"
@@ -139,7 +178,7 @@ const AuthPage: NextPage = ({}) => {
                         {getFormErrorMessage('password')}
                     </div>
 
-                    <Button type="submit" label="Войти" className="mt-2"/>
+                    <Button type="submit" label="Войти" className="mt-2" onClick={handleLogIn}/>
                 </form>
                 <p className="text-center">Нет учётной записи? <a
                     className="font-bold text-primary cursor-pointer"
@@ -207,7 +246,7 @@ const AuthPage: NextPage = ({}) => {
                             согласен с условиями и положениями*</label>
                     </div>
 
-                    <Button type="submit" label="Зарегистрироваться" className="mt-2"/>
+                    <Button type="submit" label="Зарегистрироваться" className="mt-2" onClick={handleSignUp}/>
                 </form>
                 <p className="text-center">Уже есть учетная запись? <a
                     className="font-bold text-primary cursor-pointer"
@@ -218,36 +257,38 @@ const AuthPage: NextPage = ({}) => {
     }
 
     return (
-        <MainLayout isHideHeader={true}>
-            <div className="flex fixed" style={{top: 40, right: 40}}>
-                <Button icon="pi pi-times" className="p-button-rounded p-button-secondary p-button-outlined w-2rem h-2rem"
-                        aria-label="Cancel" onClick={handleClosePage}/>
-            </div>
-            <div>
-                {
-                    typeView === TypeAuthView.SIGN_UP ?
-                        <Dialog visible={showMessage} onHide={() => setShowMessage(false)} position="center"
-                                footer={dialogFooter} showHeader={false} breakpoints={{'960px': '80vw'}}
-                                style={{width: '30vw'}}>
-                            <div className="flex align-items-center flex-column pt-6 px-3">
-                                <i className="pi pi-check-circle"
-                                   style={{fontSize: '5rem', color: 'var(--green-500)'}}></i>
-                                <h2>Регистрация завершена!</h2>
-                                <p style={{lineHeight: 1.5, textIndent: '1rem'}}>
-                                    <b>{formData.name}</b>, Ваша учетная запись зарегистрирована.
-                                    Пожалуйста, проверьте <b>{formData.email}</b> для активации аккаунта.
-                                </p>
-                            </div>
-                        </Dialog> : <></>
-                }
+        <MainLayout isHideHeader={true} className="min-h-screen">
+                <div className="min-h-screen flex justify-content-center">
+                    <div className="flex fixed" style={{top: 40, right: 40}}>
+                        <Button icon="pi pi-times" className="p-button-rounded p-button-secondary p-button-outlined w-2rem h-2rem"
+                                aria-label="Cancel" onClick={handleClosePage}/>
+                    </div>
+                    <div className="align-self-center min-h-full">
+                        {
+                            typeView === TypeAuthView.SIGN_UP ?
+                                <Dialog visible={showMessage} onHide={() => setShowMessage(false)} position="center"
+                                        footer={dialogFooter} showHeader={false} breakpoints={{'960px': '80vw'}}
+                                        style={{width: '30vw'}}>
+                                    <div className="flex align-items-center flex-column pt-6 px-3">
+                                        <i className="pi pi-check-circle"
+                                           style={{fontSize: '5rem', color: 'var(--green-500)'}}/>
+                                        <h2>Регистрация завершена!</h2>
+                                        <p style={{lineHeight: 1.5, textIndent: '1rem'}}>
+                                            <b>{formData.name}</b>, Ваша учетная запись зарегистрирована.
+                                            Пожалуйста, проверьте <b>{formData.email}</b> для активации аккаунта.
+                                        </p>
+                                    </div>
+                                </Dialog> : <></>
+                        }
 
-                <div className="flex justify-content-center">
-                    {
-                        typeView === TypeAuthView.SIGN_IN ?
-                            ViewSignIn() : ViewSignUP()
-                    }
+                        <div className="flex justify-content-center">
+                            {
+                                typeView === TypeAuthView.SIGN_IN ?
+                                    ViewSignIn() : ViewSignUP()
+                            }
+                        </div>
+                    </div>
                 </div>
-            </div>
         </MainLayout>
     )
 }
