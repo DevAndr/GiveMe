49e0a5142902f18b1eaf42ab7c0c6cc6b696f262
Носенко Илья <pitbullstar96@yandex.ru>
2023-05-09 21:43:24 +0300

    правки версия либ и конфиг некста

--- next.config.js
+++ next.config.js
@@ -1,7 +1,24 @@
+const path = require('path')
+const withSass = require('@zeit/next-sass');
+
+module.exports = withSass({
+  cssModules: true
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  env: {
+    BACKEND_HOST: process.env.BACKEND_HOST,
+    HOST_GRAPHQL: process.env.BACKEND_HOST,
+    SECRET_CLIENT: process.env.SECRET_CLIENT,
+    CLIENT_ID: process.env.CLIENT_ID,
+    REDIRECT_URL: process.env.REDIRECT_URL
+  }
 }
 
 module.exports = nextConfig
