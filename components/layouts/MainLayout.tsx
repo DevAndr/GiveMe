fb4940993b47764b04c826cfd5ca9cd3883c1329
Nosenko Ilya <pitbullstar96@gmail.com>
2023-06-18 16:06:03 +0300

    update layout

--- components/layouts/MainLayout.tsx
+++ components/layouts/MainLayout.tsx
@@ -1,10 +1,11 @@
 import Footer from '../footer'
-import style  from '../../styles/MainLayout.module.scss'
+import style from '../../styles/MainLayout.module.scss'
 import React, {FC, ReactNode, useEffect, useState} from "react"
 import NavBarRight from "../nav/NavBarRight";
-import Toolbar from "../nav/toolbar";
+import AppBar from "../nav/AppBar";
 import AuthDialog from "../dialogs/AuthDialog";
 import {useBreakpoint} from "../../hooks/breakpoint";
+import Meta, { IMeta } from "../../seo/Meta";
 
 export interface MainLayoutProps {
     children: ReactNode
@@ -13,10 +14,13 @@ export interface MainLayoutProps {
     isHideHeader?: boolean
     isHideFooter?: boolean
     isPresentView?: boolean
+    meta: IMeta
 }
 
-const MainLayout: FC<MainLayoutProps> = ({children, className, isHideMenu,
-                                             isHideHeader, isHideFooter, isPresentView}) => {
+const MainLayout: FC<MainLayoutProps> = ({
+                                             children, className, isHideMenu, meta,
+                                             isHideHeader, isHideFooter, isPresentView
+                                         }) => {
     const breakpoints: any = useBreakpoint();
     const [show, setShow] = useState(false)
 
@@ -25,22 +29,22 @@ const MainLayout: FC<MainLayoutProps> = ({children, className, isHideMenu,
         setShow(!show)
     }
 
-    console.log(breakpoints)
-
     return (
-        <>
+        <Meta {...meta}>
             {
                 !isHideHeader && <>
-                    <Toolbar onClickMenu={handleClickMenu} isHideLeftMenu={isHideMenu} isPresentView={isPresentView}/>
-                    <NavBarRight toggle={show} handleHide={() => {setShow(false)}}/>
+                    <AppBar onClickMenu={handleClickMenu} isHideLeftMenu={isHideMenu} isPresentView={isPresentView}/>
+                    <NavBarRight toggle={show} handleHide={() => {
+                        setShow(false)
+                    }}/>
                 </>
             }
-            <main style={show ? {marginLeft: '22rem'} : {}} className={`${style.mainLayout} ${className ? className : ''}`}>{children}</main>
+            <main style={show ? {marginLeft: '22rem'} : {}} className={style.mainLayout}>{children}</main>
             <AuthDialog/>
             {
                 !isHideFooter && <Footer/>
             }
-        </>
+        </Meta>
     )
 }
 
