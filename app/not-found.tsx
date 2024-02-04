493b07d2d5a52af7b8412c1048d07b957cb9bd58
Илья Носенко <pitbullstar96@gmail.com>
2024-02-04 22:45:38 +0300

    fix: added global styles, page not found, components

---
+++ app/not-found.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import Content from '@/components/wrapper/Content';
+
+export default function NotFound() {
+    return (
+       <Content typeView={'fullScreen'}>
+           <div className='flex flex-column'>
+               <h1>Страница не найдена</h1>
+               <p>Не удалось получить доступ к этой странице</p>
+               <Link href="/">Вернуться на главную</Link>
+           </div>
+       </Content>
+    )
+}
---
+++ components/navigation/NavBar.tsx
@@ -0,0 +1,82 @@
+import React, {FC, useEffect, useRef, useState} from 'react';
+import {Sidebar} from 'primereact/sidebar';
+import {Menu} from 'primereact/menu';
+
+interface IMenuItem {
+    label?: string
+    icon?: string
+    separator?: true
+}
+
+interface IMenu {
+    label?: string
+    items?: IMenuItem[]
+    separator?: true
+}
+
+const menuItems: IMenu[] = [
+    {
+        label: "Списки желаний",
+        items: [
+            {
+                label: "Сладости",
+                icon: "pi pi-book"
+            },
+            {
+                label: "Шмотки",
+                icon: "pi pi-book"
+            },
+            {
+                label: "Для секс игр",
+                icon: "pi pi-book"
+            }
+        ]
+    },
+    {
+        separator: true
+    },
+    {
+        label: "Настройки",
+        items: [
+            {
+                label: "Профиль",
+                icon: "pi pi-user-edit"
+            },
+            {
+                label: "Уведомления",
+                icon: "pi pi-bell"
+            },
+            {
+                label: "Безопасность",
+                icon: "pi pi-shield"
+            }
+        ]
+    }
+]
+interface NavBarProps {
+    toggle: boolean
+    handleHide: () => void
+}
+
+const NavBar: FC<NavBarProps> = ({toggle, handleHide}) => {
+
+    const refMenu = useRef(null);
+    const [visibleLeft, setVisibleLeft] = useState(toggle);
+
+    useEffect(() => {
+        console.log('NavBar')
+        setVisibleLeft(toggle)
+    }, [toggle])
+
+    return (
+        <Sidebar modal={false} className="p-sidebar-sm" visible={visibleLeft} onHide={handleHide} showCloseIcon={false}>
+            <h5 style={{fontWeight: 'normal'}}>Панель управления</h5>
+
+            <div className="card">
+                <Menu className="w-full" model={menuItems}/>
+            </div>
+        </Sidebar>
+    )
+};
+
+export default NavBar;
