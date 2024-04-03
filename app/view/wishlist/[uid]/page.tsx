44c2d643098fcc3a4955102ef39611fd47ac2d79
Илья Носенко <42933864+DevAndr@users.noreply.github.com>
2024-04-03 15:10:43 +0300

    fix: inject widget btn and title page

--- app/view/wishlist/[uid]/page.tsx
+++ app/view/wishlist/[uid]/page.tsx
@@ -1,10 +1,13 @@
 import WishListPublic from "@/components/wishlist/WishListPublic";
+import GoToCartWidget from "@/components/widget/GoToCartWidget";
+import PageTitle from "@/components/pageTitle/PageTitle";
 
 export default function PagePublicWishList() {
     return (
         <div className='page'>
-            <h3>Username мечтает о</h3>
+            <h3 className='pageTitle'>Username мечтает о</h3>
             <WishListPublic />
+            <GoToCartWidget/>
         </div>
     )
 }
