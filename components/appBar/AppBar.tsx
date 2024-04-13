fd9859a9ac09e94955f7f6f954a6cf5a033dd3bb
Илья Носенко <pitbullstar96@gmail.com>
2024-04-13 22:03:40 +0300

    feat: добавил меню для appbar

--- components/appBar/AppBar.tsx
+++ components/appBar/AppBar.tsx
@@ -6,7 +6,14 @@ import Logo from "@/components/logo/Logo";
 import {Avatar} from "primereact/avatar";
 import NavBar from '@/components/navigation/NavBar';
 import {Button} from 'primereact/button';
-import './style.scss'
+import './style.scss';
+import {useAuth} from '@/providers/auth/AuthProvider';
+import {useRouter} from 'next/navigation';
+import {Menubar} from 'primereact/menubar';
+import MenuAppBar from '@/components/appBar/MenuAppBar';
+import { CiViewList } from 'react-icons/ci';
+import {FaRegChartBar} from 'react-icons/fa6';
+import {FaChartLine, FaRegListAlt} from 'react-icons/fa';
 
 type AppBarType = 'dashboard' | 'default'
 
@@ -16,6 +23,8 @@ interface AppBarProps {
 }
 
 const AppBar: FC<AppBarProps> = ({type, isAuthenticated}) => {
+    const router = useRouter();
+    const {isAuth} = useAuth();
     const [show, setShow] = useState(false);
     // const isHideLeftMenu = false;
 
@@ -26,22 +35,43 @@ const AppBar: FC<AppBarProps> = ({type, isAuthenticated}) => {
     const onClickMenu = () => {
         setShow(prevState => !prevState);
     };
+    const hideMenuHandle = () => {
+        setShow(false);
+    };
 
-    const LeftElements = <div className='leftElements'>
-        <Button className='menuBtn' aria-label="Меню" icon="pi pi-bars" onClick={onClickMenu}/>
+    const authHandle = () => {
+        router.push('/auth');
+    };
+
+
+    const LeftElements = <div className="leftElements">
+        <Button className="menuBtn" aria-label="Меню" icon="pi pi-bars" onClick={onClickMenu}/>
         <Logo/>
-    </div>
+    </div>;
 
-    const RightElements = <>
-        <Avatar icon="pi pi-user" size="normal" shape="circle"/>
-    </>
+    const RightElements = <div className="rightElements">
+        <MenuAppBar items={[
+            {
+                label: 'Списки желаний',
+                link: '/wish-lists',
+                // icon: <FaRegListAlt />
+            },
+            {
+                label: 'Статистика',
+                link: '/statistics',
+                // icon: <FaChartLine />
+            },
+        ]}/>
+        {
+            isAuth ? <Avatar className="profile" icon="pi pi-user" size="normal" shape="circle"/> :
+                <Button className="btn-auth" size="small" label="Войти" icon="pi pi-sign-in" onClick={authHandle}/>
+        }
+    </div>;
 
     return (
         <>
             <Toolbar className="p-3 appBar" start={LeftElements} end={RightElements}/>
-            <NavBar handleHide={() => {
-                setShow(false);
-            }} toggle={show}/>
+            <NavBar handleHide={hideMenuHandle} toggle={show}/>
         </>
     );
 };
