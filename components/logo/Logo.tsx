448fb302b338d839aaa4f5f3fe362b76d973554f
Илья Носенко <pitbullstar96@gmail.com>
2024-04-13 22:06:56 +0300

    fix: лого

--- components/logo/Logo.tsx
+++ components/logo/Logo.tsx
@@ -1,5 +1,6 @@
 import React, {FC} from 'react';
 import Link from "next/link";
+import './styles.scss'
 
 interface LogoProps {
 
@@ -7,7 +8,7 @@ interface LogoProps {
 
 const Logo: FC<LogoProps> = () => {
     return (
-        <Link href={'/'} className='flex gap-2 align-items-center'>
+        <Link href={'/'} className='logo'>
             <i className="pi pi-gift"/>
             <h6 className='m-0 p-0'>Give Me</h6>
         </Link>
