03edc141992a4f2e9e57874cdd4c5751a02aeaa6
Илья Носенко <pitbullstar96@gmail.com>
2024-05-10 12:51:16 +0300

    refactor: крточка товара

--- components/card/WishCard.tsx
+++ components/card/WishCard.tsx
@@ -7,30 +7,38 @@ import './styles.scss';
 import Image from "next/image";
 import Likes from "@/components/likes/Likes";
 import CartBtn from "@/components/btn/CartBtn";
-import { IoGiftSharp } from 'react-icons/io5';
+import {IoGiftSharp} from 'react-icons/io5';
+import {Product} from '@/graphql/types';
+import {Button} from 'primereact/button';
 
 interface WishCardProps {
-    data: ItemWIshList;
+    keyList: string;
+    selectedProducts: string[];
+    setSelectedProducts: (value: string) => void;
+    data: Product;
 }
 
-const WishCard: FC<WishCardProps> = ({data}) => {
-    const isGifted = true;
-    const giver = 'Hella'
+const WishCard: FC<WishCardProps> = ({data, keyList, selectedProducts, setSelectedProducts}) => {
+    const isGifted = !!data.idSender;
+    const giver = 'Hella';
+
     const HeaderCard = () => {
         return (<div>
             {data.name}
         </div>);
     };
 
+
     return (
         <Card className="card wish">
             <div className="header">
                 <div className="wrap-header">
-                    <Image className="img" src={data.image} alt={data.name} width={200} height={200}/>
+                    <Image className="img" src={data.img} alt={data.name} width={200} height={200}
+                           onError={(e: any) => e.target.src = '/images/placeholder.png'}/>
                     {
-                        isGifted ? <div className='gift-label'>
-                            <IoGiftSharp className='icon'/>
-                        </div> :  <CartBtn/>
+                        isGifted && <div className="gift-label">
+                            <IoGiftSharp className="icon"/>
+                        </div>
                     }
                 </div>
             </div>
@@ -38,7 +46,11 @@ const WishCard: FC<WishCardProps> = ({data}) => {
                 <h6 className="title">{data.name}</h6>
                 <div className="description">{data.description}</div>
                 {
-                    !isGifted ? <div className="price">{data.price} ₽</div> :
+                    !isGifted ? <div className="flex justify-between items-center">
+                            <div className="price">{data.price} ₽</div>
+                            <CartBtn keyList={keyList} isSelected={!!selectedProducts.find(e => e === data.id)}
+                                     productId={data.id} onSelectedProduct={setSelectedProducts}/>
+                        </div> :
                         <div className="gift">
                             {
                                 giver ? <>Подарил <b>{giver}</b></> : <>Подарено</>
@@ -46,9 +58,6 @@ const WishCard: FC<WishCardProps> = ({data}) => {
                         </div>
                 }
             </div>
-            {/*<div className="footer">*/}
-            {/*    <Likes/>*/}
-            {/*</div>*/}
         </Card>
     )
         ;
