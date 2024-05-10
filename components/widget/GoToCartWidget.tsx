700d392a32015c020b78ab22d5224be8eb808267
Илья Носенко <pitbullstar96@gmail.com>
2024-05-10 12:56:02 +0300

    refactor: виджет карзины со счетчиком

--- components/widget/GoToCartWidget.tsx
+++ components/widget/GoToCartWidget.tsx
@@ -1,17 +1,29 @@
 import React, {FC} from 'react';
 import {Button} from "primereact/button";
-import './styles.scss'
+import './styles.scss';
+import {Badge} from 'primereact/badge';
+import {useMutation} from '@apollo/client';
+import {PATH_ORDER} from '@/graphql';
 
 interface GoToCartWidgetProps {
-
+    count: number;
+    onClick: () => void;
 }
 
-const GoToCartWidget: FC<GoToCartWidgetProps> = () => {
+const GoToCartWidget: FC<GoToCartWidgetProps> = ({count, onClick}) => {
+
+
     return (
-        <div className='wrap-widget'>
-            <Button className='btn-confirm' label={'Оформить'} icon={'pi pi-gift'}/>
+        <div className="wrap-widget">
+            <div className="fixed">
+                {
+                    count > 0 &&
+                    <Badge value={count} severity="danger" className="badge"/>
+                }
+                <Button className="btn-confirm" label={'Оформить'} icon={'pi pi-gift'} onClick={onClick}/>
+            </div>
         </div>
     );
-}
+};
 
 export default GoToCartWidget;
