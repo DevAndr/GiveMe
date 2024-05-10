01524d68de5dcc7d2340a2e554fe4636c5c1810c
Илья Носенко <pitbullstar96@gmail.com>
2024-05-10 12:58:05 +0300

    refactor: мультичекбокс

--- components/input/MultiCheckbox.tsx
+++ components/input/MultiCheckbox.tsx
@@ -1,8 +1,7 @@
 import React, {FC, useState} from 'react';
 import {MultiStateCheckbox} from 'primereact/multistatecheckbox';
 import {ACCESS_TYPE} from '@/graphql/types';
-
-
+import './styles.scss'
 
 interface IAccess {
     value: ACCESS_TYPE
@@ -23,15 +22,15 @@ const MultiCheckbox: FC<MultiCheckboxProps> = ({value, onChange}) => {
     const [access, setAccess] = useState<ACCESS_TYPE>(value && value || ACCESS_TYPE.PUBLIC);
 
     return (
-        <div className="field-checkbox m-0 flex-row-reverse">
-            <MultiStateCheckbox className="ml-2" value={access} options={options} optionValue="value"
+        <div className="field-checkbox">
+            <MultiStateCheckbox id='check-multi' className="ml-2" value={access} options={options} optionValue="value"
                                 onChange={(e) => {
                                     e.stopPropagation()
                                     const curVal = e.value === null ? ACCESS_TYPE.PUBLIC : e.value
                                     onChange(curVal)
                                     setAccess(curVal)
                                 }}/>
-            <label className="text-xs">{access}</label>
+            <label htmlFor="check-multi" className="text-xs">{access}</label>
         </div>
     )
 };
