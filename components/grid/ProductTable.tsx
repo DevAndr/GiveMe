a4e1cf271ffed5b84394fd7da53b7e46002df7f6
Илья Носенко <pitbullstar96@gmail.com>
2024-05-10 13:00:33 +0300

    refactor: таблица с товарами

--- components/grid/ProductTable.tsx
+++ components/grid/ProductTable.tsx
@@ -4,71 +4,74 @@ import {DataTable} from 'primereact/datatable';
 import {InputTextarea} from 'primereact/inputtextarea';
 import MarketPlace from '@/components/market/MarketPlace';
 import {InputText} from 'primereact/inputtext';
-import { Image } from 'primereact/image';
+import {useUpdateEditorProducts} from '@/graphql';
+import Image from 'next/image';
 
 interface ProductTableProps {
-    selections: any[]
-    value?: any[]
+    selections: any[];
+    value?: any[];
+    onSelectionChange: (e: any) => void;
 }
 
-const ProductTable: FC<ProductTableProps> = ({selections, value}) => {
-    const handleSelectionChange = (e: any) => {
-        if (e.type === 'checkbox' || e.type === 'all') {
-            setSelectedProducts(e.value)
-        }
-    }
+const ProductTable: FC<ProductTableProps> = ({selections, value, onSelectionChange}) => {
+    const updateProduct = useUpdateEditorProducts()
 
     const handleRowEditComplete = async (e) => {
-        let {newData, index} = e
-        await updateProduct(newData.uidWishList, newData)
-
-        // refetchProducts({uidWishList: currentWishList.uid})
-    }
+        let {newData, index} = e;
+        await updateProduct(newData.idWishList, newData);
+    };
 
     const imageBodyTemplate = (rowData: any) => {
-        console.log(rowData.img)
+        return <Image src={rowData.img} alt={rowData.name} className="w-4rem object-contain" width={96} height={96}  
+        onError={(e: any) => e.target.src = '/images/placeholder.png'}/> 
+    };
 
-        // return <Image src={rowData.img} alt={'photo'} width={'96'} height={'96'}/>
-        return <img src={rowData.img}
-                    onError={(e: any) => e.target.src = 'https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png'}
-                    alt={rowData.image} className="w-4rem"/>
+    const nameBodyTemplate = (rowData: any) => {
+        return <div className={`text-balance${rowData.staus === 'COMPLETED' ? ' line-through' : ''}`}>
+            {rowData.name}
+        </div>
     }
 
     const inputTextEditor = (props, field) => {
         return <InputText className="w-full" type="text" value={props.rowData[field]}
                           onChange={(e) => props.editorCallback(e.target.value)}/>;
-    }
+    };
 
     const nameEditor = (props) => {
         return inputTextEditor(props, 'name');
-    }
+    };
 
     const descriptionEditor = (options) => {
         return <InputTextarea className="w-full" value={options.value}
                               onChange={(e) => options.editorCallback(e.target.value)} rows={5}
-                              cols={30}/>
-    }
+                              cols={30}/>;
+    };
 
     const marketTypeTemplate = (rowData: any) => {
-        return <MarketPlace type={rowData.marketPlace}/>
-    }
+        return <MarketPlace type={rowData.marketPlace}/>;
+    };
+
+    const isRowSelectable = (event) => {
+        return (event.data ? event.data.status !== 'COMPLETED' : true)
+    };
 
     return (
         <DataTable editMode="row"
-                   dataKey="uid"
+                   dataKey="id"
                    resizableColumns emptyMessage={'Нет товаров в списке'}
-                   value={value} paginator rows={10} first={0}
+                   value={value} paginator rows={5} first={0}
                    selection={selections}
-                   onSelectionChange={handleSelectionChange}
+                   // isDataSelectable={isRowSelectable}
+                   onSelectionChange={onSelectionChange}
                    onRowEditComplete={handleRowEditComplete}
-                   scrollable scrollHeight="flex" responsiveLayout="scroll"
+                   scrollable scrollHeight="flex"
                    className="table-products" size="small">
             <Column selectionMode="multiple" headerStyle={{width: '.1rem'}}
                     exportable={false} style={{flex: '3rem 0 0'}}/>
             <Column field="img" body={imageBodyTemplate}
                     style={{flex: '0 0 8rem'}}/>
             <Column field="name" bodyStyle={{fontWeight: 500}} editor={nameEditor}
-                    header="Имя" sortable style={{flex: '0 0 10rem'}}/>
+                    header="Имя" sortable style={{flex: '0 0 10rem'}} body={nameBodyTemplate}/>
             <Column field="description" header="Описание"
                     editor={descriptionEditor}/>
             <Column field="marketPlace" header="Магазин" body={marketTypeTemplate}
