b4caae4120999b62aa662d22c2c90f2f30ad106d
Илья Носенко <pitbullstar96@gmail.com>
2024-05-10 12:56:31 +0300

    refactor: кнопка добавления в корзину

--- components/btn/CartBtn.tsx
+++ components/btn/CartBtn.tsx
@@ -1,31 +1,51 @@
-import React, {FC, useState} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import {Button} from "primereact/button";
+import LocalCartService from '@/services/LocalCartService';
 
 interface CartBtnProps {
-
+    productId: string;
+    keyList: string;
+    isSelected?: boolean;
+    onSelectedProduct: (id: string) => void;
 }
 
 type StateBtn = 'ADDED' | 'NONE' | 'LOADING' | 'STOCK'
 
-const CartBtn: FC<CartBtnProps> = () => {
-    const [state, setState] = useState<StateBtn>('NONE')
+const CartBtn: FC<CartBtnProps> = ({keyList, productId, isSelected, onSelectedProduct}) => {
+    const [state, setState] = useState<StateBtn>(  isSelected ? 'ADDED' : 'NONE');
+
+    // useEffect(() => {
+    //     if (isSelected) {
+    //         setState('ADDED');
+    //     } else {
+    //         setState('NONE');
+    //     }
+    // }, [isSelected]);
+
+
 
     const addInToCartHandler = async () => {
         if (state === 'NONE') {
-            setState('LOADING')
-            await setTimeout(() => setState('ADDED'), 1500)
+            setState('LOADING');
+            setTimeout(() => {
+                onSelectedProduct(productId);
+                setState('ADDED');
+            }, 1500);
         }
 
         if (state === 'ADDED') {
-            setState('LOADING')
-            await setTimeout(() => setState('NONE'), 300)
+            setState('LOADING');
+            setTimeout(() => {
+                onSelectedProduct(productId);
+                setState('NONE');
+            }, 300);
         }
-    }
+    };
 
     return (
-        <Button className='btn-add' icon={`pi ${state === 'NONE' ? 'pi-cart-plus' : 'pi-check'}`}
+        <Button className="btn-add" icon={`pi ${state === 'NONE' ? 'pi-cart-plus' : 'pi-check'}`}
                 loading={state === 'LOADING'} onClick={addInToCartHandler}/>
     );
-}
+};
 
 export default CartBtn;
